aae5603b176440406fdc596870691705e93b39e8
Hélène <delrangarr@cy-tech.fr>
2023-05-22 16:02:04 +0200

    update folder

--- class/database_login.php
+++ class/database_login.php
@@ -1,10 +1,9 @@
 <?php
 
-class database_login
-{
-    public static string $hostname = "localhost";
-    public static string $username = "root";
-    public static string $password = "";
-    public static string $database = "titre";
-    public static int $port = 8006;
-}
+// Données de connexion à la base de données 
+$servername = 'localhost';
+$username = 'root'; // Mettre votre username MySQL
+$password = ''; // Mettre votre mot de passe MySQL
+$bddname = 'projetIaPau';
+$port = 3306
+?>
