1f0e09a6242d7574e78064665e907fb38180c1c3
m-firmino <123177531+m-firmino@users.noreply.github.com>
2023-05-24 15:36:17 +0200

    profil css update

--- pages/profilEtudiant.php
+++ pages/profilEtudiant.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <?php
 if (isset($_SESSION['email'])) {
 
@@ -10,132 +9,116 @@ if (isset($_SESSION['email'])) {
     $prenomUtilisateur = $infos[0]['prenomUtilisateur'];
 
     ?>
-    <html lang="en">
-<!--
-    <head>
-        
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="/css/profil.css" />
-        <script defer src="../scripts/profil.js"></script>
-
-
-        <title>Profil</title>
-    </head>
--->
- 
-    <body>
-
-        <div class="prenomUtilisateur">
-            <h2> Nom : </h2>
-            <?php
-            echo "<h2>" . $prenomUtilisateur . "</h2>";
-            ?>
-        </div>
-
-        <div class="nomUtilisateur">
-            <h2> Prénom : </h2>
-            <?php
-            echo "<h2>" . $infos[0]['nomUtilisateur'] . "</h2>";
-            ?>
-        </div>
-
-        <div class="email">
-            <h2> Email : </h2>
-            <?php
-            echo "<h2>" . $infos[0]['email'] . "</h2>";
-            ?>
-        </div>
-
-        <div class="numeroTel">
-            <h2> Numéro de téléphone : </h2>
-            <?php
-            echo "<h2>" . $infos[0]['numeroTel'] . "</h2>";
-            ?>
-        </div>
-
-        <div class="niveauEtude">
-            <h2> Niveau d'étude : </h2>
-            <?php
-            echo "<h2>" . $infos[0]['niveauEtude'] . "</h2>";
-            ?>
-        </div>
-
-        <div class="ecole">
-            <h2> Ecole : </h2>
-            <?php
-            echo "<h2>" . $infos[0]['ecole'] . "</h2>";
-            ?>
-        </div>
-
-        <div class="ville">
-            <h2> Ville : </h2>
-            <?php
-            echo "<h2>" . $infos[0]['ville'] . "</h2>";
-            ?>
-        </div>
-        
-        <div class="modifier">
-            <li class="buttonC" class="boutonmodifier">
-                <div onclick="openModifierModal()">Modifier vos informations</div>
-            </li>
-
-        </div>
-
-        <div class="projet_etudiant">
-            <h2> Vos projets : </h2>
-
-        </div>
     
-    </body>
 
-    </html>
 
-    <div id="modifierModal">
-    <div class="modal-content">
-        <span id="closeModifier" onclick="closeModifierModal()">&times;</span>
-        <h2>Modifier vos informations</h2>
-        <form id="modifierForm" action="../action/edit_profil.php" method="POST">
-            <label for="prenomUtilisateur">Prénom:</label><br>
-            <input type="text" id="prenomUtilisateur" name="prenomUtilisateur"><br>
 
-            <label for="nomUtilisateur">Nom:</label><br>
-            <input type="text" id="nomUtilisateur" name="nomUtilisateur"><br>
+<div class="prenomUtilisateur">
+    <h2> Nom : </h2>
+    <?php
+    echo "<h2>" . $prenomUtilisateur . "</h2>";
+    ?>
+</div>
 
-            <label for="email">Email:</label><br>
-            <input type="email" id="email" name="email"><br>
+<div class="nomUtilisateur">
+    <h2> Prénom : </h2>
+    <?php
+    echo "<h2>" . $infos[0]['nomUtilisateur'] . "</h2>";
+    ?>
+</div>
 
-            <label for="numeroTel">Numéro de téléphone:</label><br>
-            <input type="tel" id="numeroTel" name="numeroTel"><br>
+<div class="email">
+    <h2> Email : </h2>
+    <?php
+    echo "<h2>" . $infos[0]['email'] . "</h2>";
+    ?>
+</div>
 
-            <div class="radioniveauEtude">
-            <label for="niveauEtude" class="titreniveauEtude">niveauEtude</label>
-            <input type="radio" id="niveauEtude" name="niveauEtude" value="L1" > L1 
-            <input type="radio" id="niveauEtude" name="niveauEtude" value="L2" > L2
-            <input type="radio" id="niveauEtude" name="niveauEtude" value="L3" > L3
-            <input type="radio" id="niveauEtude" name="niveauEtude" value="M1" > M1
-            <input type="radio" id="niveauEtude" name="niveauEtude" value="M2" > M2
-            <input type="radio" id="niveauEtude" name="niveauEtude" value="D" > D
+<div class="numeroTel">
+    <h2> Numéro de téléphone : </h2>
+    <?php
+    echo "<h2>" . $infos[0]['numeroTel'] . "</h2>";
+    ?>
+</div>
 
-            <label for="ecole">Ecole:</label><br>
-            <input type="text" id="ecole" name="ecole"><br>
+<div class="niveauEtude">
+    <h2> Niveau d'étude : </h2>
+    <?php
+    echo "<h2>" . $infos[0]['niveauEtude'] . "</h2>";
+    ?>
+</div>
 
-            <label for="ville">Ville:</label><br>
-            <input type="text" id="ville" name="ville"><br>
+<div class="ecole">
+    <h2> Ecole : </h2>
+    <?php
+    echo "<h2>" . $infos[0]['ecole'] . "</h2>";
+    ?>
+</div>
 
-            <label for="AncienPassword">Ancien mot de passe:</label><br>
-            <input type="password" id="AncienPassword" name="AncienPassword"><br>
+<div class="ville">
+    <h2> Ville : </h2>
+    <?php
+    echo "<h2>" . $infos[0]['ville'] . "</h2>";
+    ?>
+</div>
 
-            <label for="password">Nouveau mot de passe:</label><br>
-            <input type="password" id="password" name="password"><br>
+<div class="modifier">
+    <li class="buttonC" class="boutonmodifier">
+        <div onclick="openModifierModal()">Modifier vos informations</div>
+    </li>
 
-            <label for="confirm_password">Confirmer nouveau mot de passe:</label><br>
-            <input type="password" id="confirm_password" name="confirm_password"><br>
-            
-            <input type="submit" value="Valider">
-        </form>
-    </div>
+</div>
+
+<div class="projet_etudiant">
+    <h2> Vos projets : </h2>
+
+</div>
+    
+
+<div id="modifierModal">
+<div class="modal-content">
+    <span id="closeModifier" onclick="closeModifierModal()">&times;</span>
+    <h2>Modifier vos informations</h2>
+    <form id="modifierForm" action="../action/edit_profil.php" method="POST">
+        <label for="prenomUtilisateur">Prénom:</label><br>
+        <input type="text" id="prenomUtilisateur" name="prenomUtilisateur"><br>
+
+        <label for="nomUtilisateur">Nom:</label><br>
+        <input type="text" id="nomUtilisateur" name="nomUtilisateur"><br>
+
+        <label for="email">Email:</label><br>
+        <input type="email" id="email" name="email"><br>
+
+        <label for="numeroTel">Numéro de téléphone:</label><br>
+        <input type="tel" id="numeroTel" name="numeroTel"><br>
+
+        <div class="radioniveauEtude">
+        <label for="niveauEtude" class="titreniveauEtude">niveauEtude</label>
+        <input type="radio" id="niveauEtude" name="niveauEtude" value="L1" > L1 
+        <input type="radio" id="niveauEtude" name="niveauEtude" value="L2" > L2
+        <input type="radio" id="niveauEtude" name="niveauEtude" value="L3" > L3
+        <input type="radio" id="niveauEtude" name="niveauEtude" value="M1" > M1
+        <input type="radio" id="niveauEtude" name="niveauEtude" value="M2" > M2
+        <input type="radio" id="niveauEtude" name="niveauEtude" value="D" > D
+
+        <label for="ecole">Ecole:</label><br>
+        <input type="text" id="ecole" name="ecole"><br>
+
+        <label for="ville">Ville:</label><br>
+        <input type="text" id="ville" name="ville"><br>
+
+        <label for="AncienPassword">Ancien mot de passe:</label><br>
+        <input type="password" id="AncienPassword" name="AncienPassword"><br>
+
+        <label for="password">Nouveau mot de passe:</label><br>
+        <input type="password" id="password" name="password"><br>
+
+        <label for="confirm_password">Confirmer nouveau mot de passe:</label><br>
+        <input type="password" id="confirm_password" name="confirm_password"><br>
+        
+        <input type="submit" value="Valider">
+    </form>
+</div>
 
 
 
--- pages/profilGestionnaire.php
+++ pages/profilGestionnaire.php
@@ -10,107 +10,90 @@ if (isset($_SESSION['email'])) {
     $prenomUtilisateur = $infos[0]['prenomUtilisateur'];
 
     ?>
-    <html lang="en">
-<!--
-    <head>
-        
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="/css/profil.css" />
-        <script defer src="../scripts/profil.js"></script>
 
 
-        <title>Profil</title>
-    </head>
--->
- 
-    <body>
+<div class="prenomUtilisateur">
+    <h2> Nom : </h2>
+    <?php
+    echo "<h2>" . $prenomUtilisateur . "</h2>";
+    ?>
+</div>
 
-        <div class="prenomUtilisateur">
-            <h2> Nom : </h2>
-            <?php
-            echo "<h2>" . $prenomUtilisateur . "</h2>";
-            ?>
-        </div>
+<div class="nomUtilisateur">
+    <h2> Prénom : </h2>
+    <?php
+    echo "<h2>" . $infos[0]['nomUtilisateur'] . "</h2>";
+    ?>
+</div>
 
-        <div class="nomUtilisateur">
-            <h2> Prénom : </h2>
-            <?php
-            echo "<h2>" . $infos[0]['nomUtilisateur'] . "</h2>";
-            ?>
-        </div>
+<div class="email">
+    <h2> Email : </h2>
+    <?php
+    echo "<h2>" . $infos[0]['email'] . "</h2>";
+    ?>
+</div>
 
-        <div class="email">
-            <h2> Email : </h2>
-            <?php
-            echo "<h2>" . $infos[0]['email'] . "</h2>";
-            ?>
-        </div>
+<div class="numeroTel">
+    <h2> Numéro de téléphone : </h2>
+    <?php
+    echo "<h2>" . $infos[0]['numeroTel'] . "</h2>";
+    ?>
+</div>
 
-        <div class="numeroTel">
-            <h2> Numéro de téléphone : </h2>
-            <?php
-            echo "<h2>" . $infos[0]['numeroTel'] . "</h2>";
-            ?>
-        </div>
+<div class="entreprise">
+    <h2> Entreprise : </h2>
+    <?php
+    echo "<h2>" . $infos[0]['nomEntreprise'] . "</h2>";
+    ?>
+</div>
 
-        <div class="entreprise">
-            <h2> Entreprise : </h2>
-            <?php
-            echo "<h2>" . $infos[0]['nomEntreprise'] . "</h2>";
-            ?>
-        </div>
+<div class="dateFinUtilisateur">
+    <h2> Date de fin d'utilisateur : </h2>
+    <?php
+    echo "<h2>" . $infos[0]['dateFinUtilisateur'] . "</h2>";
+    ?>
+</div>
 
-        <div class="dateFinUtilisateur">
-            <h2> Date de fin d'utilisateur : </h2>
-            <?php
-            echo "<h2>" . $infos[0]['dateFinUtilisateur'] . "</h2>";
-            ?>
-        </div>
-        
-        <div class="modifier">
-            <li class="buttonC" class="boutonmodifier">
-                <div onclick="openModifierModal()">Modifier vos informations</div>
-            </li>
+<div class="modifier">
+    <li class="buttonC" class="boutonmodifier">
+        <div onclick="openModifierModal()">Modifier vos informations</div>
+    </li>
 
-        </div>
+</div>
 
-        <div class="projet_gestionnaire">
-            <h2> Vos projets à gérer: </h2>
+<div class="projet_gestionnaire">
+    <h2> Vos projets à gérer: </h2>
 
-        </div>
+</div>
     
-    </body>
 
-    </html>
 
-    <div id="modifierModal">
-    <div class="modal-content">
-        <span id="closeModifier" onclick="closeModifierModal()">&times;</span>
-        <h2>Modifier vos informations</h2>
-        <form id="modifierForm" action="../action/edit_profil.php" method="POST">
-            <label for="prenomUtilisateur">Prénom:</label><br>
-            <input type="text" id="prenomUtilisateur" name="prenomUtilisateur"><br>
-            <label for="nomUtilisateur">Nom:</label><br>
-            <input type="text" id="nomUtilisateur" name="nomUtilisateur"><br>
-            <label for="email">Email:</label><br>
-            <input type="email" id="email" name="email"><br>
-            <label for="numeroTel">Numéro de téléphone:</label><br>
-            <input type="tel" id="numeroTel" name="numeroTel"><br>
-            <label for="nomEntreprise">Entreprise:</label><br>
-            <input type="text" id="nomEntreprise" name="nomEntreprise"><br>
-            <label for="dateFinUtilisateur">Date de fin d'utilisateur:</label><br>
-            <input type="date" id="dateFinUtilisateur" name="dateFinUtilisateur"><br>
-            <label for="AncienPassword">Ancien mot de passe:</label><br>
-            <input type="password" id="AncienPassword" name="AncienPassword"><br>
-            <label for="password">Nouveau mot de passe:</label><br>
-            <input type="password" id="password" name="password"><br>
-            <label for="confirm_password">Confirmer nouveau mot de passe:</label><br>
-            <input type="password" id="confirm_password" name="confirm_password"><br>
-            <input type="submit" value="Valider">
-        </form>
-    </div>
+<div id="modifierModal">
+<div class="modal-content">
+    <span id="closeModifier" onclick="closeModifierModal()">&times;</span>
+    <h2>Modifier vos informations</h2>
+    <form id="modifierForm" action="../action/edit_profil.php" method="POST">
+        <label for="prenomUtilisateur">Prénom:</label><br>
+        <input type="text" id="prenomUtilisateur" name="prenomUtilisateur"><br>
+        <label for="nomUtilisateur">Nom:</label><br>
+        <input type="text" id="nomUtilisateur" name="nomUtilisateur"><br>
+        <label for="email">Email:</label><br>
+        <input type="email" id="email" name="email"><br>
+        <label for="numeroTel">Numéro de téléphone:</label><br>
+        <input type="tel" id="numeroTel" name="numeroTel"><br>
+        <label for="nomEntreprise">Entreprise:</label><br>
+        <input type="text" id="nomEntreprise" name="nomEntreprise"><br>
+        <label for="dateFinUtilisateur">Date de fin d'utilisateur:</label><br>
+        <input type="date" id="dateFinUtilisateur" name="dateFinUtilisateur"><br>
+        <label for="AncienPassword">Ancien mot de passe:</label><br>
+        <input type="password" id="AncienPassword" name="AncienPassword"><br>
+        <label for="password">Nouveau mot de passe:</label><br>
+        <input type="password" id="password" name="password"><br>
+        <label for="confirm_password">Confirmer nouveau mot de passe:</label><br>
+        <input type="password" id="confirm_password" name="confirm_password"><br>
+        <input type="submit" value="Valider">
+    </form>
+</div>
 
 
 
