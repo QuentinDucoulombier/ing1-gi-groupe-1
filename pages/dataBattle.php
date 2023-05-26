77e805d87b0bd3c2da1d57eae367c302466bc0e4
Hélène <delrangarr@cy-tech.fr>
2023-05-26 23:53:58 +0200

    add css

--- pages/dataBattle.php
+++ pages/dataBattle.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="styles/component/evenements.css" />
+
 <?php
 
 
@@ -14,39 +16,53 @@ if(isset($_GET['battle'])){
 
 
     echo '<h1>'. $nomBattle . '</h3>';
-    echo '<p>'. $nomChallenge . '</p>';
+    echo '<p>date</p>';
 
     $projet = getProjetData($nomBattle);
 
-    echo '<div class=projet>';
+    echo '  <div class=projet>';
 
-        echo ' <div class = "title">';
-        echo '      <h2>'. $projet[0]['nomProjet'] . '</h2>';
-        echo ' </div>';
-        echo ' <div class = "description">';
-        echo        $projet[0]['description'];
-        echo ' </div>';
+        echo '      <div class = "en-tete">';
 
-        $gestionnaire = getSuperviseur($projet[0]['nomProjet']);
-        echo ' <div class="contact-projet">';
-        echo '      <h3> Coordonnées contact </h3>';
-        echo '      <div class = "contact-gestionnaire">';
-        echo '          <p>'.$gestionnaire[0]['prenomUtilisateur'].' ' . $gestionnaire[0]['nomUtilisateur'].'</p>';
-        echo '          <p> Mail : ' .$gestionnaire[0]['email'] .' Tel :'. $gestionnaire[0]['numeroTel'] .'</p>';
+        echo '          <div class = "desc">';
+        echo '              <div class = "title">';
+        echo '                  <h2>'. $projet[0]['nomProjet'] . '</h2>';
+        echo '              </div>';
+
+        echo '              <div class = "description">';
+        echo                    $projet[0]['description'];
+        echo '              </div>';
+        echo '          </div>';
+
         echo '      </div>';
-        echo ' </div>';
-        echo ' <div class="ressources-projet">';
-        echo '      <h3> Ressources du projet </h3>';
-        echo '      <p>URL d\'accès aux fichiers de description et des données du data challenge: </p>';
-        echo '      <a href ='.$projet['urlFichier'].'>Description du projet</a>';
-        echo '      <p>URL vidéo de présentation du projet.</p>';
-        echo '      <a href ='.$projet['urlVideo'].'>Vidéo du projet</a>';
-        echo ' </div>';
-        echo ' <div>';
-        echo '          <img src="'.$projet['imageEvent'].'" alt="'.$projet['nomEvenement'].'">';
+
+        echo '          <div class=image-projet>';
+        echo '              <img src="'.$projet[0]['imageEvent'].'" alt="'.$projet[0]['nomEvenement'].'">';
+        echo '          </div>';
+
+
+        $gestionnaires = getSuperviseur($projet[0]['nomProjet']);
+
+        echo '      <div class="contact-projet">';
+        echo '          <h3> Coordonnées contact </h3>';
+
+        foreach ($gestionnaires as $gestionnaire){
+            echo '      <div class = "contact-gestionnaire">';
+            echo '          <p>'.$gestionnaire['prenomUtilisateur'].' ' . $gestionnaire['nomUtilisateur'].'</p>';
+            echo '          <p> Mail : ' .$gestionnaire['email'] .' Tel :'. $gestionnaire['numeroTel'] .'</p>';
+            echo '      </div>';
+        }
         echo '      </div>';
+
+        echo '      <div class="ressources-projet">';
+        echo '          <h3> Ressources du projet </h3>';
+        echo '          <p>URL d\'accès aux fichiers de description et des données du data challenge: </p>';
+        echo '          <a href ='.$projet[0]['urlFichier'].'>Description du projet</a>';
+        echo '          <p>URL vidéo de présentation du projet.</p>';
+        echo '          <a href ='.$projet[0]['urlVideo'].'>Vidéo de présentation du projet</a>';
         echo '      </div>';
-    echo '</div>';
+
+        echo '  </div>';
 
 
             
