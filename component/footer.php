e3442e861adad1325a3010c39b0def64e3815cb9
Hélène <delrangarr@cy-tech.fr>
2023-05-30 15:50:38 +0200

    footer and accueil

--- component/footer.php
+++ component/footer.php
@@ -1,4 +1,16 @@
 <footer>
+
+    <div>
+        <a href="">Nous contacter</a>
+    </div>
+
+    <div>
+        <a href="">IA Pau</a>
+    </div>
+
+    <div>
+        <a href="">Nos challenges</a>
+    </div>
     
-    <p>Footer</p>
+
 </footer>
