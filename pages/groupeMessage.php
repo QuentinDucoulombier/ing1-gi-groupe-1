73d3cb89c918b3e9d4f7e1ccb64eead15c212e50
ducoulombi <ducoulombi@cy-tech.fr>
2023-06-01 17:07:40 +0200

    add css

--- pages/groupeMessage.php
+++ pages/groupeMessage.php
@@ -1,27 +1,31 @@
-<?php
+<link rel="stylesheet" href="styles/component/groupMessage.css" />
 
-    if (isset($_SESSION['email']))
-    {
-        $user = getUser($_SESSION['email']);
+<div id="groupMessage">
+    <?php
 
-        if ($user[0]['type'] == "Administrateur" || ($user[0]['type'] == "Gestionnaire" && checkGestionnaireProjet($user[0]['email'], $battle['nomEvenement']) )) {
+        if (isset($_SESSION['email']))
+        {
+            $user = getUser($_SESSION['email']);
 
-            $idTeam = $_POST["idTeam"];
-            $nameEquipe = getNameGroup($idTeam);
-            echo '
-            <h1>Message groupé a l\'équipe '.$nameEquipe["nomEquipe"].'</h1>
-            <form action="../action/chat/newGroupMessage.php" method="POST">
-                <input type="hidden" name="idEquipe" value="'.$idTeam.'"/>
-                <label for="message">Votre message</label>
-                <input type="text" name="message" />
-                <input type="submit" value="Envoie message"/>
+            if ($user[0]['type'] == "Administrateur" || ($user[0]['type'] == "Gestionnaire" && checkGestionnaireProjet($user[0]['email'], $battle['nomEvenement']) )) {
 
-            </form>
-            ';
-        }else {
+                $idTeam = $_POST["idTeam"];
+                $nameEquipe = getNameGroup($idTeam);
+                echo '
+                <h1>Message groupé a l\'équipe '.$nameEquipe["nomEquipe"].'</h1>
+                <form action="../action/chat/newGroupMessage.php" method="POST">
+                    <input type="hidden" name="idEquipe" value="'.$idTeam.'"/>
+                    <label for="message">Votre message</label>
+                    <input type="text" name="message" />
+                    <input type="submit" value="Envoie message"/>
+
+                </form>
+                ';
+            }else {
+                header ('Location: /?page=404');
+            }
+        }else{
             header ('Location: /?page=404');
         }
-    }else{
-        header ('Location: /?page=404');
-    }
-?>
+    ?>
+</div>
