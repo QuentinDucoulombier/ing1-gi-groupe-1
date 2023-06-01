53cc24811c717cdca1f9a3e5200ecabc2a1bbf2d
ducoulombi <ducoulombi@cy-tech.fr>
2023-06-01 22:06:33 +0200

    clean code 2

--- scripts/viewTeam.js
+++ scripts/viewTeam.js
@@ -2,7 +2,7 @@ function supprimerMember(idUser,idTeam){
     xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
+            //console.log(this.responseText);
             window.location.href = "/?page=accueil";
         }
             
